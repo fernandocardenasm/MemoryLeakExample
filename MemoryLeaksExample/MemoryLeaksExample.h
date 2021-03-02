f7cac68aacbf507dcc4ca0262b6321ba1897f691
Fernando Cardenas <fernando.cardenas@grandcentrix.net>
2021-03-02 11:36:50 +0100

    Initial Commit

---
+++ MemoryLeaksExample/MemoryLeaksExample.h
@@ -0,0 +1,18 @@
+//
+//  MemoryLeaksExample.h
+//  MemoryLeaksExample
+//
+//  Created by Fernando Cardenas on 02.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MemoryLeaksExample.
+FOUNDATION_EXPORT double MemoryLeaksExampleVersionNumber;
+
+//! Project version string for MemoryLeaksExample.
+FOUNDATION_EXPORT const unsigned char MemoryLeaksExampleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MemoryLeaksExample/PublicHeader.h>
+
+
